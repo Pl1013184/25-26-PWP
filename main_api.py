6ce18120d6a63bf95eeaf1169d39d2f1e6492539
Pl1013184 <pl1013184@ahschool.com>
2026-02-01 10:56:54 -0500

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -8,79 +8,69 @@ from threading import Thread
 app = Flask(__name__)
 global videio
 global bholdr
-bholdr=np.zeros((1440,640*3,3),dtype=np.uint8)
+bholdr=[None]*5
+print(bholdr)
 lock=threading.Lock()
 videio = cv2.VideoCapture(0)
-
+print("Width:", videio.get(cv2.CAP_PROP_FRAME_WIDTH))
+print("Height:", videio.get(cv2.CAP_PROP_FRAME_HEIGHT))
 videio.set(cv.CAP_PROP_FRAME_WIDTH,640)
-videio.set(cv.CAP_PROP_FRAME_HEIGHT,1440)
-while videio.read()[0]==False:
-   pass
+videio.set(cv.CAP_PROP_FRAME_HEIGHT,480)
+#while videio.read()[0]==False:
+#   pass
 print("Frames Per Second",videio.get(cv.CAP_PROP_FPS))
 def gen_frm():
-    global raw
     global bholdr
     i=0
     while True:
-        lock.acquire()
         x,raw=videio.read()
+        lock.acquire()
+        
         if not x:
             lock.release()
             continue
- #       if i%50==0:
-           # print(type(raw.copy()),type(raw))
-        i+=1
-#        print(raw)
-#        print(raw.shape,raw.dtype)
         if raw is None:
             lock.release()
             continue
-       # bholdr=np.zeros((720,1280,3),dtype=np.uint8)
-       # print(bholdr)
-        bholdr=np.append(bholdr,raw)
-        bholdr=np.delete(bholdr,range(raw.size))
-#        print(bholdr)
-        bholdr=bholdr.reshape(1440,640*3,3)
-#        print(bholdr)
-#        cv.imshow('test',bholdr)
+        bholdr[i]=raw
         lock.release()
-#        time.sleep(5)
+        i+=1
+        if i==4:
+            i=0
 t=Thread(target=gen_frm,daemon=True)
 raw=None
 
 t.start()
-while raw is None:
-  pass
+#while raw is None:
+#  pass
 
 def gen_vid():
-  global raw
   global bholdr
  # print(type(bholdr[1]))
   while True:
        lock.acquire()
-       if bholdr is None:
-            continue
-       raw1=np.asarray(bholdr[:160,:480]).copy()
-      # print('thinking')
- # print(type(raw1))
- #      print(type(bholdr[1]))
-       raw1=cv.resize(raw1,(640,480)) 
+       if bholdr[3] is None:
+           lock.release()
+           continue
+       raw1=bholdr[3].copy()        
        lock.release()
+       raw1=cv.resize(raw1,(640,480))
       # print('thought')
        x, img_data = cv2.imencode('.jpg', raw1)
        raw_bytes = img_data.tobytes()
+       time.sleep(0.01)
        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + raw_bytes + b'\r\n')
 
 
 def gen_proc_vid():
-  global raw
   while True:
       lock.acquire()
-      if raw is None:
-           continue
-      og= bholdr[:160,:480].copy()
-      raw1 = cv2.Canny(bholdr[:160,:480].copy(),100,200)
+      if bholdr[3] is None:
+          lock.release()
+          continue
+      og=bholdr[3].copy()        
       lock.release()
+      raw1 = cv2.Canny(og,100,200)
      # raw1=cv.adaptiveThreshold(raw1,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
      # lines = cv.HoughLinesP(raw1,rho=1,theta=np.pi/180,threshold=100 ,minLineLength=100,maxLineGap=12)
       lines = cv.HoughLines(raw1,1,np.pi/180,150, None,0,0)
@@ -105,7 +95,9 @@ def gen_proc_vid():
       rhol=0
       rhor=0
       theta=0
+      #cv.line(og,(0,0),(640,480),(255,0,0),7)
       if len(parallel)==0:
+          print('No parallel lines found')
           x, img_data = cv2.imencode('.jpg', og)
           raw_bytes = img_data.tobytes()
           yield (b'--frame\r\n'
@@ -151,7 +143,13 @@ def gen_proc_vid():
       x2 = int(x0 - 1000*(-b))
       y2 = int(y0 - 1000*(a))
       cv.line(og,(x1,y1),(x2,y2),(0,0,255),5)
+      gray=cv.cvtColor(og,cv.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+      contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+      cv.ap
+      cv2.drawContours(og, contours, -1, (0,255,255), 2)
       
+      time.sleep(0.01)
 #      break
 #      raw2=cv.addWeighted(frame,0.7,imag,0.3,0)
       raw2=og 
@@ -166,7 +164,7 @@ def video():
     return  Response(gen_vid(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/proc_vid')
 def proc_vid():
-    return Response(gen_proc_vid(), mimetype='multipart/x-mixed-replace;boundary=frame')
+    return Response(gen_proc_vid(), mimetype='multipart/x-mixed-replace; boundary=frame')
     #gen_proc_vid()
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
