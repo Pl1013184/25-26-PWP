5e66b5494dc514a530d20488238e91639fb355c8
JeremyChen3 <pl1019492@ahschool.com>
2026-03-03 08:17:08 -0500

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -1,11 +1,11 @@
-
-from flask import Flask, Response
+from flask import Flask, Response,request,render_template
 import cv2
 import cv2 as cv
 import numpy as np
 import threading
 import time
 from threading import Thread
+import pi_client
 app = Flask(__name__)
 global videio
 global bholdr
@@ -18,8 +18,8 @@ print("Height:", videio.get(cv2.CAP_PROP_FRAME_HEIGHT))
 videio.set(cv.CAP_PROP_FRAME_WIDTH,640)
 videio.set(cv.CAP_PROP_FRAME_HEIGHT,480)
 #videio.set(cv.CAP_PROP_FPS,30)
-while videio.read()[0]==False:
-   pass
+#while videio.read()[0]==False:
+#   pass
 print("Frames Per Second",videio.get(cv.CAP_PROP_FPS))
 def gen_frm():
     global bholdr
@@ -27,7 +27,7 @@ def gen_frm():
     while True:
         x,raw=videio.read()
         lock.acquire()
-        
+        #1
         if not x:
             lock.release()
             continue
@@ -61,7 +61,7 @@ def gen_vid():
        if bholdr[3] is None:
            lock.release()
            continue
-       raw1=bholdr[3].copy()        
+       raw1=bholdr[3].copy()
        lock.release()
        raw1=cv.resize(raw1,(640,480))
       # print('thought')
@@ -69,7 +69,12 @@ def gen_vid():
        raw_bytes = img_data.tobytes()
        time.sleep(0.01)
        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + raw_bytes + b'\r\n')
-
+def showimg(og):
+    x, img_data = cv2.imencode('.jpg', og)
+    raw_bytes = img_data.tobytes()
+    return (b'--frame\r\n'
+          b'Content-Type: image/jpeg\r\n\r\n' + raw_bytes +b'\r\n')
+    time.sleep(0.01) 
 
 def gen_proc_vid():
   while True:
@@ -79,39 +84,57 @@ def gen_proc_vid():
           continue
       og=bholdr[3].copy()        
       lock.release()
-      frame=og.copy()
-      #raw1 = cv2.Canny(og,100,200)
      # raw1=cv.adaptiveThreshold(raw1,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
      # lines = cv.HoughLinesP(raw1,rho=1,theta=np.pi/180,threshold=100 ,minLineLength=100,maxLineGap=12)
-      #lines = cv.HoughLines(raw1,1,np.pi/180,150, None,0,0)
-      #raw1= cv.cvtColor(raw1, cv.COLOR_GRAY2BGR)
-      elmt=cv.getStructuringElement(cv.MORPH_RECT,(3,3))    
+     # imag = raw1
+      
+      #cv.circle(og,(150,75),5,(255,0,0),-1)
+      #cv.circle(og,(400,75),5,(255,0,0),-1)
+      #cv.circle(og,(0,480),5,(255,0,0),-1)
+      #cv.circle(og,(640,480),5,(255,0,0),-1)
+      #cv.circle(og,(0,380),5,(255,0,0),-1)
+      rows,cols,ch = og.shape
+      pts1 = np.float32([[150,75],[400,75],[640,480],[0,480]])
+      pts2 = np.float32([[150,75],[400,75],[400,480],[150,480]])
+      M = cv.getPerspectiveTransform(pts1,pts2)
+      og = cv.warpPerspective(og,M,(640,480))
+      frame = og.copy()
+      dil=cv.getStructuringElement(cv.MORPH_RECT, (5,5))
       og=cv.cvtColor(og,cv.COLOR_BGR2HSV)
       h,s,v=cv.split(og)
-      v[:,:]=245
-      h[:,:]=0
+      v[:,:]=255
       og=cv.merge((h,s,v))
       og=cv.cvtColor(og,cv.COLOR_HSV2BGR)
-      gray=cv.cvtColor(og,cv.COLOR_BGR2GRAY)
-      og=cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY_INV,11,2)
-      og=cv.equalizeHist(og)
-      og=cv.GaussianBlur(og,(5,5),15)
-      og=cv.erode(og,elmt)
-      #og=cv.Canny(og,100,600)
-      lines=cv.HoughLines(og,1,np.pi/180,150,None,0,0)
-      test=True
-      if lines is not None or test:
-          x, img_data = cv2.imencode('.jpg', og)
-          raw_bytes = img_data.tobytes()
-          yield (b'--frame\r\n'
-          b'Content-Type: image/jpeg\r\n\r\n' + raw_bytes +b'\r\n')
-          continue  
+      og=cv.cvtColor(og,cv.COLOR_BGR2GRAY)
+      og=cv.GaussianBlur(og,(17,17),7)
+      og=cv.dilate(og,dil,iterations=3)
+      #og=cv.adaptiveThreshold(og,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,3,-5)
+      #og= cv.Canny(og,150,200)
+      lines = cv.HoughLines(og,1,np.pi/180,50,None,0,0)
+      #print(len(lines))
+      Test=True
+      if lines is None or Test:
+             yield (showimg(frame))
+             continue
+      og=frame
       parallel=[]
       thresh=20
       athresh=10
       for i in lines:
            rho,theta = i[0]
            for  l in lines:
+               '''
+               if l[0][1]-np.pi/180<(10*np.pi/180):
+                   a = np.cos(l[0][1])
+                   b = np.sin(l[0][1])
+                   x0 = a*l[0][0]
+                   y0 = b*l[0][0]
+                   x1 = int(x0 + 1000*(-b))
+                   y1 = int(y0 + 1000*(a))
+                   x2 = int(x0 - 1000*(-b))
+                   y2 = int(y0 - 1000*(a))
+                   cv.line(og,(x1,y1),(x2,y2),(255,255,0),5)
+               el'''
                if (l[0][0]-rho>thresh) and l[0][1]-theta<(athresh*np.pi/180):
                    parallel.append([(rho,l[0][0]),theta])
       rho=0
@@ -134,11 +157,11 @@ def gen_proc_vid():
           y1 = int(y0 + 1000*(a))
           x2 = int(x0 - 1000*(-b))
           y2 = int(y0 - 1000*(a))
-          cv.line(frame,(x1,y1),(x2,y2),(255,0,0),5)
+          cv.line(og,(x1,y1),(x2,y2),(255,0,0),5)
           for par in parallel:
               rho1,rho2=par[0]
               rhol+=max(par[0])
-              rhor+=min(par[0])
+              rhol+=min(par[0])
           rhol*=1/len(parallel)
           rhor*=1/len(parallel)
           a = np.cos(theta)
@@ -149,7 +172,7 @@ def gen_proc_vid():
           y1 = int(y0 + 1000*(a))
           x2 = int(x0 - 1000*(-b))
           y2 = int(y0 - 1000*(a))
-          cv.line(frame,(x1,y1),(x2,y2),(0,255,0),5)
+          cv.line(og,(x1,y1),(x2,y2),(0,255,0),5)
           a = np.cos(theta)
           b = np.sin(theta)
           x0 = a*rhor
@@ -158,16 +181,18 @@ def gen_proc_vid():
           y1 = int(y0 + 1000*(a))
           x2 = int(x0 - 1000*(-b))
           y2 = int(y0 - 1000*(a))
-          cv.line(frame,(x1,y1),(x2,y2),(0,0,255),5)
-      #gray=cv.cvtColor(og,cv.COLOR_BGR2GRAY)
+          cv.line(og,(x1,y1),(x2,y2),(0,0,255),5)
+          Test=True
+           
+          '''
+      gray=cv.cvtColor(og,cv.COLOR_BGR2GRAY)
       gray = cv.GaussianBlur(gray,(5,5), 1)
+      
       ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
       contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
       #https://pyimagesearch.com/2021/10/06/opencv-contour-approximation/
       clist=[]
       for c in contours:
-          if cv.arcLength(c,False)>800:
-              continue
           for eps in np.linspace(0.001, 0.05, 10):
 	# approximate the contour
               peri = cv2.arcLength(c, True)
@@ -178,21 +203,20 @@ def gen_proc_vid():
               text = "eps={:.4f}, num_pts={}".format(eps, len(approx)) 
               if len(approx)==15:
                   clist.append(approx)
-          cv.drawContours(frame, [approx],-1,(0,244,144),5)
+          cv.drawContours(og, [approx],-1,(0,244,144),5)
           #clist.append(approx)
       if clist!=[]:
-          avg=sumofc(clist)/len(clist)
+          avg=sum(clist)/len(clist)
           avg=avg.reshape(-1,1,2)
           avg= avg.astype(np.int32)
       #print(avg[:,0,:])
-          cv.drawContours(frame,[avg],-1,(125,234,34),7)
-      
-      
-            
+          cv.drawContours(og,[avg],-1,(125,234,34),7)
+      '''
       time.sleep(0.001)
 #      break
-#      raw2=cv.addWeighted(frame,0.7,imag,0.3,0)
-      raw2=frame
+      #raw2=cv.addWeighted(frame,0.7,og,0.3,0)
+      raw2=og
+      print('hi')
       x, img_data = cv2.imencode('.jpg', raw2)
       raw_bytes = img_data.tobytes()
      # l.release()
@@ -205,10 +229,18 @@ def video():
 @app.route('/proc_vid')
 def proc_vid():
     return Response(gen_proc_vid(), mimetype='multipart/x-mixed-replace; boundary=frame')
-    #gen_proc_vid()
 @app.route("/log")
 def log():
-    pass
-
+	try: 
+	    open = open("log.txt","x") 
+	    open.close()
+	except:pass
+	write= open("log.txt","a")
+	write.write(request.args.get("logdata"))
+	write.close()
+@app.route("/controls/<dir>",methods=['POST'])
+def mover(dir):
+	pi_client.execute_command(dir)
+	return render_template('gui.html')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True,debug=False)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
