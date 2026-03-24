4d1a1f4d7f8e83bc03e4bdce6263e1d69a6a12c1
Pl1013184 <pl1013184@ahschool.com>
2026-03-24 08:32:51 -0400

    Update PBear-outline.py

--- PBear-outline.py
+++ PBear-outline.py
@@ -1,8 +1,20 @@
 import ALL_Files
-
+import motor_steering
+def Penguin(direction:str):
+	if direction== 'left':
+		motor_steering.set_motor_speeds(-10)
+	elif direction=='right':
+		motor_steering.set_motor_speeds(10)
 def motor_turn(turn_angle:int):
+	#Using dot turn
 	pass
         #Create function with testing.
 def main(angle:degrees):
 	if (angle>45) or (angle<-45):
 		motor_turn(angle/2)
+	elif angle>0:
+		Penguin(left)
+	elif angle<0:
+		Penguin(right)
+	else:
+		pass
